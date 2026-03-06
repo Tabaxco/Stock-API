ab0bb036b91e8b4244dd3be2cebae260288ee80a
Tabaxco <rodriguesgui48@gmail.com>
2026-03-06 15:08:27 -0300

    yes: This is the standard message of my committing script!

--- src/main/java/com/produtoapi/controller/ProdutoController.java
+++ src/main/java/com/produtoapi/controller/ProdutoController.java
@@ -1,4 +1,42 @@
 package com.produtoapi.controller;
 
+import com.produtoapi.model.Produto;
+import com.produtoapi.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
-}
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public List<Produto> listarTodos() {
+        return produtoService.listarTodos();
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto) {
+        return produtoService.salvar(produto);
+    }
+
+    @PutMapping("/{id}")
+    public Produto atualizar (@PathVariable long id, @RequestBody Produto produto) {
+        return produtoService.atualizar(id, produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable long id) {
+        produtoService.deletar(id);
+    }
+
+    @GetMapping("/{id}")
+        public Optional<Produto> findById(@PathVariable long id) {
+            return produtoService.findById(id);
+        }
+    }
+
